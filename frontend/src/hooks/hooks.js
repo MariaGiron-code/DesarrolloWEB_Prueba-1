681282729809bce0f3c8effc8c5248d0c1625445
Josué Ortiz <168014672+NW08@users.noreply.github.com>
2025-11-11 16:52:24 -0500

    Delete frontend directory

--- frontend/src/hooks/hooks.js
+++
@@ -1,9 +0,0 @@
-// importar y exportar todos los 
-// hooks personalizados desde un solo archivo para buenas prácticas
-export { usePlayers } from './usePlayers';
-export { usePlayerStats } from './usePlayerStats';
-export { useVideos } from './useVideos';
-export { usePlayerSearch } from './usePlayerSearch';
-
-
-export { usePlayers as default } from './usePlayers';
